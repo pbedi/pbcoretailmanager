3aa52c1e14cf9fd519c5c418d31abe9fdc643abb
Philip <philip.bedi@berkeleygroup.co.uk>
2020-09-07 18:18:32 +0100

    Tables created

---
+++ PBRMData/dbo/Tables/Sale.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sale]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CashierId] VARCHAR(128) NOT NULL, 
+    [SaleDate] DATETIME2 NOT NULL, 
+    [SubTotal] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    [Total] MONEY NOT NULL 
+)
