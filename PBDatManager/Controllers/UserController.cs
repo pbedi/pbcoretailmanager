d1450e2c66d9e089c2a808136346c6a68727d2f6
Philip <philip.bedi@berkeleygroup.co.uk>
2020-11-28 14:58:07 +0000

    part 1 of user data

---
+++ PBDatManager/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using PBDataManager.Library.DataAccess;
+using PBDataManager.Library.Internal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace PBDatManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+            
+            return data.GetUserById(userId);
+        }
+
+        
+    }
+}
---
+++ PBDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,21 @@
+﻿using PBDataManager.Library.Inernal.DataAccess;
+using PBDataManager.Library.Internal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string UserId)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+            var p = new { Id = UserId };
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "PBRMData");
+            return output;
+        }
+    }
+}
